edb2a021cf0f20252264fcc0ed0177a4e75f7caf
Sanry_Wazaa <122481803+Santiproxd@users.noreply.github.com>
2023-08-04 22:58:21 -0300

    Delete assets/preload/data/Endless-Sleep directory

--- assets/preload/data/Endless-Sleep/Drain.lua
+++
@@ -1,8 +0,0 @@
-function opponentNoteHit()
-    health = getProperty('health')
-	if curStep >= 272 and curStep < 9176 then
-    if getProperty('health') > 0.4 then
-        setProperty('health', health- 0.028);
-    end
-end
-end
